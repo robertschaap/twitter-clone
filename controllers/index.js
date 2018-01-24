82c655bbd05bf05d40ca1e88523fba86f0b5f3b0
robertschaap <robert.schaap@mac.com>
2018-01-24 19:40:06 +0100

    nottweet iteration

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { trendingNotTweets, notTweets })
 });
 
 module.exports = router;
+
+const trendingNotTweets = Array(5).fill({ 
+    hashtag: '#codingbreak', 
+    tweetCount: 1000
+})
+
+const notTweets = Array(20).fill({
+    userName: 'Robert Schaap',
+    userHandle: '@colorcodedcode',
+    notTweetBody: 'My cats freak out when I play air guitar #codingbreak'
+})
